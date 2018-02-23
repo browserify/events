88d6f03828c795c51eee0994c34ec01df5cb6874
Renée Kooi <renee@kooi.me>
2018-02-23 10:25:42 +0100

    Disable __proto__ test in IE8

--- tests/special-event-names.js
+++ tests/special-event-names.js
@@ -12,29 +12,32 @@ assert.strictEqual(ee.eventNames().length, 0);
 assert.strictEqual(ee._events.hasOwnProperty, undefined);
 assert.strictEqual(ee._events.toString, undefined);
 
-ee.on('__proto__', handler);
 ee.on('__defineGetter__', handler);
 ee.on('toString', handler);
+ee.on('__proto__', handler);
 
-console.log(ee.eventNames().join(','))
-assert.strictEqual(ee.eventNames().length, 3);
-assert.strictEqual(ee.eventNames()[0], '__proto__');
-assert.strictEqual(ee.eventNames()[1], '__defineGetter__');
-assert.strictEqual(ee.eventNames()[2], 'toString');
+assert.strictEqual(ee.eventNames()[0], '__defineGetter__');
+assert.strictEqual(ee.eventNames()[1], 'toString');
 
-assert.strictEqual(ee.listeners('__proto__').length, 1);
-assert.strictEqual(ee.listeners('__proto__')[0], handler);
 assert.strictEqual(ee.listeners('__defineGetter__').length, 1);
 assert.strictEqual(ee.listeners('__defineGetter__')[0], handler);
 assert.strictEqual(ee.listeners('toString').length, 1);
 assert.strictEqual(ee.listeners('toString')[0], handler);
 
-ee.on('__proto__', common.mustCall(function(val) {
-  assert.strictEqual(val, 1);
-}));
-ee.emit('__proto__', 1);
+// Disable __proto__ tests in IE8
+if (typeof navigator === 'undefined' || !/MSIE 8/.test(navigator.userAgent)) {
+  assert.strictEqual(ee.eventNames().length, 3);
+  assert.strictEqual(ee.eventNames()[2], '__proto__');
+  assert.strictEqual(ee.listeners('__proto__').length, 1);
+  assert.strictEqual(ee.listeners('__proto__')[0], handler);
 
-process.on('__proto__', common.mustCall(function(val) {
-  assert.strictEqual(val, 1);
-}));
-process.emit('__proto__', 1);
+  ee.on('__proto__', common.mustCall(function(val) {
+    assert.strictEqual(val, 1);
+  }));
+  ee.emit('__proto__', 1);
+
+  process.on('__proto__', common.mustCall(function(val) {
+    assert.strictEqual(val, 1);
+  }));
+  process.emit('__proto__', 1);
+}
