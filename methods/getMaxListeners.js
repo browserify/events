d220bff6c632d9bb4d18488d3bd67e817a98fcae
Behnam Mohammadi (بهنام محمدی) <itten@live.com>
2018-02-23 04:48:16 +0330

    fixed bug in getMaxListeners default value

--- methods/getMaxListeners.js
+++ methods/getMaxListeners.js
@@ -1,5 +1,6 @@
 module.exports = function (EventEmitter) {
-  return function getMaxListeners(that = EventEmitter) {
+  return function getMaxListeners(that) {
+    that = that || EventEmitter
     if (that._maxListeners === undefined)
       return EventEmitter.defaultMaxListeners;
     return that._maxListeners;
